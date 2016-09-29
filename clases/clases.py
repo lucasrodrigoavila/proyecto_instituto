24ea02f1f243fffb1594bab120d2fdf4a34a8624
SoniaTapia <soniatapiaaguero@gmail.com>
2016-09-29 20:00:41 -0300

    clase persona

--- clases/clases.py
+++ clases/clases.py
@@ -1,3 +1,29 @@
+
+
+
+class Persona:
+	def __init__ (self, nombre, apellido, dni, sexo):
+		self.nombre = nombre
+		self.apellido = apellido
+		self.dni = dni
+		self.sexo = sexo
+
+	def nombre(self):
+		self.nombre = self.nombre
+
+	def apellido(self):
+		self.apellido = self.apellido
+
+	def dni(self):
+		self.dni = self.dni
+
+	def sexo(self):
+		self.sexo = self.sexo
+
+	def mostrar_listado(self):
+		print ("Listado de las Persona",, self.nombre, self.apellido, self.dni, self.sexo)
+
+
 class Examen():
 	def __init__(self, fecha,aula):
 		self.fecha=fecha
