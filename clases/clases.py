aa05ca4cd4cd57d181e8fb8b2a0e04e084acaa3c
Ruben973 <manchits20126@gmail>
2016-09-29 20:48:52 -0300

    agrego clase alumno

--- clases/clases.py
+++ clases/clases.py
@@ -1,3 +1,42 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Alumno(Persona):
+    def __init__(self, materiaquecursa, aniodeingreso, matricula):
+        Persona.__init__(self, nombre, apellido, dni, sexo)
+        
+            self.materiaquecursa=materiaquecursa
+            self.aniodeingreso=aniodeingreso
+            self.matricula=matricula
+       
+    def notasdemateria(self):
+        print("Se pide las ", self.materiaquecursa, "el alumno")
+
+
+
 class Examen():
 	def __init__(self, fecha,aula):
 		self.fecha=fecha
