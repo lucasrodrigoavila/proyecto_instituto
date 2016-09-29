2f06ec8d75414aac13d0cb0cb37244c7f1a27ab8
JazminBlue <you@example.com>
2016-09-30 01:24:30 +0200

    Subiendo clase Profesor

--- clases/clases.py
+++ clases/clases.py
@@ -1,3 +1,36 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Profesor(Persona):
+    
+    def __init__(self, materia, estudios, asistencia):
+        Persona.__init__(self, nombre, apellido,dni, sexo)
+    
+            self.materia=materia
+            self.estudios=estudios
+            self.asistencia=asistencia
+
+    def tomarasistencia(self):
+                return self.asistencia
+
+
+
+
+
 class Examen():
 	def __init__(self, fecha,aula):
 		self.fecha=fecha
